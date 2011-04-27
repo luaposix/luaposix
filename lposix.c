cef67be59329123a91cce00d125c396c989ae63a
Reuben Thomas <rrt@sc3d.org>
2011-04-27 13:25:16 +0100

    Add mkstemp.

--- lposix.c
+++ lposix.c
@@ -647,6 +647,28 @@ static int Pmkfifo(lua_State *L)		/** mkfifo(path) */
 }
 
 
+static int Pmkstemp(lua_State *L)                 /** mkstemp(path) */
+{
+	const char *path = luaL_checkstring(L, 1);
+	void *ud;
+	lua_Alloc lalloc = lua_getallocf(L, &ud);
+	char *tmppath;
+	int res;
+
+	if ((tmppath = lalloc(ud, NULL, 0, strlen(path + 1))) == NULL)
+		return 0;
+	strcpy(tmppath, path);
+	res = mkstemp(path);
+
+	if (res == -1)
+		return pusherror(L, path);
+
+	lua_pushinteger(L, res);
+	lua_pushstring(L, tmppath);
+	lalloc(ud, tmppath, 0, 0);
+	return 2;
+}
+
 static int runexec(lua_State *L, int use_shell)
 {
 	const char *path = luaL_checkstring(L, 1);
@@ -1772,6 +1794,7 @@ static const luaL_reg R[] =
 	{"localtime",		Plocaltime},
 	{"mkdir",		Pmkdir},
 	{"mkfifo",		Pmkfifo},
+	{"mkstemp",             Pmkstemp},
 	{"pathconf",		Ppathconf},
 	{"pipe",		Ppipe},
 	{"raise",		Praise},
