8f74776ec9bb60ad2c5cd72247baa7c574711298
Reuben Thomas <rrt@sc3d.org>
2011-04-27 13:26:05 +0100

    Update maintainer and bump copyright year.

--- lposix.c
+++ lposix.c
@@ -1,8 +1,8 @@
 /*
 * lposix.c
 * POSIX library for Lua 5.1.
-* (c) Natanael Copa (maintainer) <natanael.copa@gmail.com> 2008-2010
-* (c) Reuben Thomas <rrt@sc3d.org> 2010
+* (c) Reuben Thomas (maintainer) <rrt@sc3d.org> 2010-2011
+* (c) Natanael Copa <natanael.copa@gmail.com> 2008-2010
 * Clean up and bug fixes by Leo Razoumov <slonik.az@gmail.com> 2006-10-11
 * Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br> 07 Apr 2006 23:17:49
 * Based on original by Claudio Terra for Lua 3.x.
