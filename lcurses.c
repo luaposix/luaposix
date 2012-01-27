958c7af049bd0966a456d81c8e4740afc64f65f5
Reuben Thomas <rrt@sc3d.org>
2012-01-27 17:27:46 +0000

    curses: update ax_with_curses.m4 to latest version.

--- lcurses.c
+++ lcurses.c
@@ -1,7 +1,7 @@
 /*************************************************************************
  * Library: lcurses - Lua 5.1 interface to the curses library            *
  *                                                                       *
- * (c) Reuben Thomas <rrt@sc3d.org> 2009-2010                            *
+ * (c) Reuben Thomas <rrt@sc3d.org> 2009-2012                            *
  * (c) Tiago Dionizio <tiago.dionizio AT gmail.com> 2004-2007            *
  *                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining *
@@ -30,10 +30,18 @@
 #include <string.h>
 #include <lua.h>
 #include <lauxlib.h>
-#ifdef HAVE_NCURSES_H
-#include <ncurses.h>
+#if defined(HAVE_NCURSESW_CURSES_H)
+#  include <ncursesw/curses.h>
+#elif defined(HAVE_NCURSESW_H)
+#  include <ncursesw.h>
+#elif defined(HAVE_NCURSES_CURSES_H)
+#  include <ncurses/curses.h>
+#elif defined(HAVE_NCURSES_H)
+#  include <ncurses.h>
+#elif defined(HAVE_CURSES_H)
+#  include <curses.h>
 #else
-#include <curses.h>
+#  error "SysV or X/Open-compatible Curses header file required"
 #endif
 #include <term.h>
 #include "lua52compat.h"
