luaposix/luaposix

commit 5aa297512e484e5c50d623feac5cd5c180433e99

lcurses.c: don't run strlen unnecessarily; w[mv]addstr does that anyway

lcurses.c
@@ -1155,10 +1155,8 @@ static int Waddstr(lua_State *L)
     WINDOW *w = checkwin(L, 1);
     const char *str = luaL_checkstring(L, 2);
     int n = luaL_optint(L, 3, -1);
-
-    if (n < 0) n = lua_strlen(L, 2);
-
     lua_pushboolean(L, B(waddnstr(w, str, n)));
+    lua_pushboolean(L, B(waddstr(w, str)));
     return 1;
 }
 
@@ -1169,9 +1167,6 @@ static int Wmvaddstr(lua_State *L)
     int x = luaL_checkint(L, 3);
     const char *str = luaL_checkstring(L, 4);
     int n = luaL_optint(L, 5, -1);
-
-    if (n < 0) n = lua_strlen(L, 4);
-
     lua_pushboolean(L, B(mvwaddnstr(w, y, x, str, n)));
     return 1;
 }

commit 91df8628af9a5737f5128cbb2e604ecf3686952a

curses: don't waddstr() each string twice.
Fix a bug introduced by commit 5aa297512.
* lcurses.c (Waddstr): Only add the string one time!

lcurses.c
@@ -1156,7 +1156,6 @@ static int Waddstr(lua_State *L)
     const char *str = luaL_checkstring(L, 2);
     int n = luaL_optint(L, 3, -1);
     lua_pushboolean(L, B(waddnstr(w, str, n)));
-    lua_pushboolean(L, B(waddstr(w, str)));
     return 1;
 }
 