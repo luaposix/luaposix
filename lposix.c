03edca77019cc4619fffa62897c2b6c9c860874c
Reuben Thomas <rrt@sc3d.org>
2013-02-11 01:16:32 +0000

    lposix.c: Reuben Thomas is no longer the maintainer.

--- lposix.c
+++ lposix.c
@@ -3,7 +3,7 @@
 */
 /*
 *  POSIX library for Lua 5.1/5.2.
-* (c) Reuben Thomas (maintainer) <rrt@sc3d.org> 2010-2012
+* (c) Reuben Thomas <rrt@sc3d.org> 2010-2012
 * (c) Natanael Copa <natanael.copa@gmail.com> 2008-2010
 * Clean up and bug fixes by Leo Razoumov <slonik.az@gmail.com> 2006-10-11
 * Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br> 07 Apr 2006 23:17:49
