37ac12f5f96bd54ce9564a0dbf4bbfe1ff39ba9b
Reuben Thomas <rrt@sc3d.org>
2013-02-15 13:54:35 +0000

    Release v5.1.24

---
+++ luaposix-5.1.24/curses.lua
@@ -0,0 +1,59 @@
+--- Lua bindings for curses
+local M = require "curses_c"
+local curses = M
+
+-- These Lua functions detect number of args, like Unified Funcs in Perl Curses
+-- see http://pjb.com.au/comp/lua/lcurses.html
+-- see http://search.cpan.org/perldoc?Curses
+
+function M.addch (...)
+  if #{...} == 3 then
+    return curses.stdscr():mvaddch(...)
+  else
+    return curses.stdscr():addch(...)
+  end
+end
+
+function M.addstr(...) -- detect number of args, like Unified Funcs in Perl Curses
+  if #{...} == 3 then
+    return curses.stdscr():mvaddstr(...)
+  else
+    return curses.stdscr():addstr(...)
+  end
+end
+
+function M.attrset (a) return curses.stdscr():attrset(a) end
+function M.clear ()    return curses.stdscr():clear() end
+function M.clrtobot () return curses.stdscr():clrtobot() end
+function M.clrtoeol () return curses.stdscr():clrtoeol() end
+
+function M.getch (...)
+  local c
+  if #{...} == 2 then
+    c = curses.stdscr():mvgetch(...)
+  else
+    c = curses.stdscr():getch()
+  end
+  if c < 256 then
+    return string.char(c)
+  end
+  -- could kludge-test for utf8, e.g. c3 a9 20  c3 aa 20  c3 ab 20  e2 82 ac 0a
+  return c
+end
+
+function M.getstr (...)
+  if #{...} > 1 then
+    return curses.stdscr():mvgetstr(...)
+  else
+    return curses.stdscr():getstr(...)
+  end
+end
+M.getnstr = M.getstr
+
+function M.getyx ()    return curses.stdscr():getyx() end
+function M.keypad (b)  return curses.stdscr():keypad(b) end
+function M.move (y,x)  return curses.stdscr():move(y,x) end
+function M.refresh ()  return curses.stdscr():refresh() end
+function M.timeout (t) return curses.stdscr():timeout(t) end
+
+return M
---
+++ luaposix-5.1.24/posix.lua
@@ -0,0 +1,131 @@
+-- Additions to the posix module (of luaposix).
+local M = require "posix_c"
+local posix = M
+local bit32 = require "bit32"
+
+--- Create a file.
+-- @param file name of file to create
+-- @param mode permissions with which to create file
+-- @return file descriptor, or -1 on error
+function M.creat (file, mode)
+  return posix.open (file, bit32.bor (posix.O_CREAT, posix.O_WRONLY, posix.O_TRUNC), mode)
+end
+
+--- Run a program like <code>os.execute</code>, but without a shell.
+-- @param file filename of program to run
+-- @param ... arguments to the program
+-- @return status exit code, or nil if fork or wait fails
+-- @return error message, or exit type if wait succeeds
+function M.system (file, ...)
+  local pid = posix.fork ()
+  if pid == 0 then
+    posix.execp (file, ...)
+    -- Only get here if there's an error; kill the fork
+    local _, no = posix.errno ()
+    posix._exit (no)
+  else
+    local pid, reason, status = posix.wait (pid)
+    return status, reason -- If wait failed, status is nil & reason is error
+  end
+end
+
+--- Check permissions like <code>access</code>, but for euid.
+-- Based on the glibc function of the same name. Does not always check
+-- for read-only file system, text busy, etc., and does not work with
+-- ACLs &c.
+-- @param file file to check
+-- @param mode checks to perform (as for access)
+-- @return 0 if access allowed; <code>nil</code> otherwise (and errno is set)
+function M.euidaccess (file, mode)
+  local pid = posix.getpid ()
+
+  if pid.uid == pid.euid and pid.gid == pid.egid then
+    -- If we are not set-uid or set-gid, access does the same.
+    return posix.access (file, mode)
+  end
+
+  local stats = posix.stat (file)
+  if not stats then
+    return
+  end
+
+  -- The super-user can read and write any file, and execute any file
+  -- that anyone can execute.
+  if pid.euid == 0 and ((not string.match (mode, "x")) or
+                      string.match (stats.st_mode, "x")) then
+    return 0
+  end
+
+  -- Convert to simple list of modes.
+  mode = string.gsub (mode, "[^rwx]", "")
+
+  if mode == "" then
+    return 0 -- The file exists.
+  end
+
+  -- Get the modes we need.
+  local granted = stats.st_mode:sub (1, 3)
+  if pid.euid == stats.st_uid then
+    granted = stats.st_mode:sub (7, 9)
+  elseif pid.egid == stats.st_gid or set.new (posix.getgroups ()):member (stats.st_gid) then
+    granted = stats.st_mode:sub (4, 6)
+  end
+  granted = string.gsub (granted, "[^rwx]", "")
+
+  if string.gsub ("[^" .. granted .. "]", mode) == "" then
+    return 0
+  end
+  posix.set_errno (EACCESS)
+end
+
+--- Add one gettimeofday() returned timeval to another.
+-- @param x a timeval
+-- @param y another timeval
+-- @return x + y, adjusted for usec overflow
+function M.timeradd (x,y)
+  local sec, usec = 0, 0
+  if x.sec then sec = sec + x.sec end
+  if y.sec then sec = sec + y.sec end
+  if x.usec then usec = usec + x.usec end
+  if y.usec then usec = usec + y.usec end
+  if usec > 1000000 then
+    sec = sec + 1
+    usec = usec - 1000000
+  end
+
+  return { sec = sec, usec = usec }
+end
+
+--- Compare one gettimeofday() returned timeval with another
+-- @param x a timeval
+-- @param y another timeval
+-- @return 0 if x and y are equal, >0 if x is newer, <0 if y is newer
+function M.timercmp (x, y)
+  local x = { sec = x.sec or 0, usec = x.usec or 0 }
+  local y = { sec = y.sec or 0, usec = y.usec or 0 }
+  if x.sec ~= y.sec then
+    return x.sec - y.sec
+  else
+    return x.usec - y.usec
+  end
+end
+
+--- Subtract one gettimeofday() returned timeval from another.
+-- @param x a timeval
+-- @param y another timeval
+-- @return x - y, adjusted for usec underflow
+function M.timersub (x,y)
+  local sec, usec = 0, 0
+  if x.sec then sec = x.sec end
+  if y.sec then sec = sec - y.sec end
+  if x.usec then usec = x.usec end
+  if y.usec then usec = usec - y.usec end
+  if usec < 0 then
+    sec = sec - 1
+    usec = usec + 1000000
+  end
+
+  return { sec = sec, usec = usec }
+end
+
+return M
