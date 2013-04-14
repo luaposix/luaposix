78294ff454be5409fdb5f7206799c1d7669de4eb
Gary V. Vaughan <gary@gnu.org>
2013-04-14 21:56:23 +0700

    slingshot: adopt slingshot release mechanism.
    * README: Move from here...
    * README.md: ...to here. Adjust for GH Markdown.
    * GNUmakefile, Makefile.am, build-aux/mkrockspecs,
    m4/ax_compare_version.m4, m4/ax_lua.m4:
    Remove. These files are handled by slingshot now.
    * rockspecs.lua, m4/ax_with_prog.m4: Remove. No longer used.
    * bootstrap.slingshot: New file. Slingshot bootstrap plugin.
    * bootstrap.conf: Source it.
    * configure.ac: Adjust to drive slingshot processes.
    * local.mk: New file. Project local make rules.
    * rockspec.conf: New file. Slingshat rockspec template.
    * specs/specs.mk: Simplified.
    * .prev-version: Record previous version number.
    * .gitignore: Update.
    * .travis.yml: Regenerate.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

---
+++ local.mk
@@ -0,0 +1,83 @@
+# Local Make rules.
+
+## ------------ ##
+## Environment. ##
+## ------------ ##
+
+std_path  = $(abs_srcdir)/?.lua;$(LUA_PATH)
+std_cpath = $(abs_builddir)/$(objdir)/?$(shrext);$(LUA_CPATH)
+
+LUA_ENV   = LUA_PATH="$(std_path)" LUA_CPATH="$(std_cpath)"
+
+
+## ---------- ##
+## Bootstrap. ##
+## ---------- ##
+
+AM_CFLAGS     = $(WERROR_CFLAGS) $(WARN_CFLAGS)
+old_NEWS_hash = unknown
+
+
+## ------------- ##
+## Declarations. ##
+## ------------- ##
+
+dist_data_DATA		=
+dist_doc_DATA		=
+
+
+## ------ ##
+## Build. ##
+## ------ ##
+
+EXTRA_LTLIBRARIES	+= curses_c.la
+lib_LTLIBRARIES		+= posix_c.la $(WANTEDLIBS)
+dist_data_DATA		+= posix.lua $(WANTEDLUA)
+
+posix_c_la_SOURCES = lposix.c
+posix_c_la_CFLAGS  = $(POSIX_EXTRA_CFLAGS)
+posix_c_la_LDFLAGS = -module -avoid-version $(POSIX_EXTRA_LDFLAGS)
+
+curses_c_la_SOURCES = lcurses.c
+curses_c_la_LDFLAGS = -module -avoid-version $(CURSES_LIB) -rpath '$(libdir)'
+
+
+## -------------- ##
+## Documentation. ##
+## -------------- ##
+
+dist_doc_DATA += 			\
+	curses.html			\
+	lcurses_c.html			\
+	docs/index.html			\
+	docs/ldoc.css			\
+	$(NOTHING_ELSE)
+
+examplesdir = $(docdir)/examples
+examples_DATA = $(wildcard examples/*.lua)
+
+doc: $(dist_doc_DATA)
+
+
+## ------------- ##
+## Distribution. ##
+## ------------- ##
+
+EXTRA_DIST +=				\
+	make_lcurses_doc.pl		\
+	strlcpy.c			\
+	lua52compat.h
+
+$(dist_doc_DATA): lcurses.c make_lcurses_doc.pl
+	$(PERL) make_lcurses_doc.pl
+if HAVE_LDOC
+	$(LDOC) .
+else
+	$(MKDIR_P) docs
+	touch docs/index.html docs/ldoc.css
+endif
+
+MAINTAINERCLEANFILES +=			\
+	docs/index.html			\
+	docs/ldoc.css			\
+	$(NOTHING_ELSE)
---
+++ specs/specs.mk
@@ -0,0 +1,18 @@
+# Specl specs make rules.
+
+
+## ------------ ##
+## Environment. ##
+## ------------ ##
+
+SPECL_ENV = $(LUA_ENV)
+
+
+## ------ ##
+## Specs. ##
+## ------ ##
+
+specl_SPECS =					\
+	$(NOTHING_ELSE)
+
+include build-aux/specl.mk
