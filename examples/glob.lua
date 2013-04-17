9fde48e8bb72ad489dc88130b6ad527130e26736
Ico Doornekamp <ico@pruts.nl>
2013-04-17 22:43:05 +0200

    Fixed glob example, added missing 'posix ='. Also removed the readlink() part because /proc on linux violates posix and does not properly fill in st_size on lstat(), causing readlink() to fail with EINVAL.

--- examples/glob.lua
+++ examples/glob.lua
@@ -1,8 +1,5 @@
-require "posix"
+local posix = require "posix"
 
 for i, j in pairs (posix.glob ("/proc/[0-9]*/exe")) do
-  local f = posix.readlink (j)
-  if f then
-    print (f)
-  end
+  print(j)
 end
