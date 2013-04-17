luaposix/luaposix

commit 8deb0ebd041ab6d4761f86a7a906dd850111bae7

poll only returns active events in revents table, increases performance

lposix.c
@@ -1061,8 +1061,10 @@ static void Ppoll_events_to_table(lua_State *L, int table, short events)
 
 	for (i = 0; i < PPOLL_EVENT_NUM; i++)
 	{
-		lua_pushboolean(L, events & Ppoll_event_map[i].bit);
-		lua_setfield(L, table, Ppoll_event_map[i].name);
+		if(events & Ppoll_event_map[i].bit) {
+			lua_pushboolean(L, events & Ppoll_event_map[i].bit);
+			lua_setfield(L, table, Ppoll_event_map[i].name);
+		}
 	}
 }
 

commit ae27dcaa33035fab25ff5d9efaee7e92c15c4611

I broke poll with commit 8deb0ebd041ab6d4761f86a7a906dd850111bae7
The result is just fine for calling poll() only once, but breaks for
subsequent calls to poll() when passing the same table. In this case,
the events are not cleared from the revents table, causing false
positives.

lposix.c
@@ -1071,10 +1071,8 @@ static void Ppoll_events_to_table(lua_State *L, int table, short events)
 
 	for (i = 0; i < PPOLL_EVENT_NUM; i++)
 	{
-		if(events & Ppoll_event_map[i].bit) {
-			lua_pushboolean(L, 1);
-			lua_setfield(L, table, Ppoll_event_map[i].name);
-		}
+		lua_pushboolean(L, events & Ppoll_event_map[i].bit);
+		lua_setfield(L, table, Ppoll_event_map[i].name);
 	}
 }
 