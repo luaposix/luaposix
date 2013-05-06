f3497865199b44d3d1357709818a77c5943e805c
Gary V. Vaughan <gary@gnu.org>
2013-05-06 14:01:16 +0700

    sanity: add configuration fragment with sanity exclusions.
    * build-aux/sanity-config.mk (sc_require_config_h)
    (sc_require_config_h_first): Exclude dummy.c and strlcpy.c, which
    are both included directly into another source file before
    compilation.
    * .gitignore, build-aux/.gitignore: Adjust.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

---
+++ build-aux/sanity-cfg.mk
@@ -0,0 +1,4 @@
+exclude_file_name_regexp--sc_require_config_h = ^(dummy.c|strlcpy.c)$$
+exclude_file_name_regexp--sc_require_config_h_first = ^(dummy.c|strlcpy.c)$$
+
+EXTRA_DIST += build-aux/sanity-cfg.mk
