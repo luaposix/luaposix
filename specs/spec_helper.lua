2a3fbf06bbde91803bb7c5d40d543d645f17d3d4
Gary V. Vaughan <gary@gnu.org>
2014-09-21 12:15:48 +0100

    posix: provide idiomatic argument errors from sockaddr calls.
    * specs/spec_helper.lua (init): New helper to return a partial
    application of expected argument error call.
    * specs/posix_spec.yaml (bind, connect, sendto): Use it to
    specify expected bad argument errors.
    * ext/posix/posix.c (try_getfield): Move from here...
    (checkfieldtype): ...to here.  Lua functions normally throw an
    error for bad arguments, so do that for posix API argument errors
    too.
    (sockaddr_from_lua): Use it.
    Provide a default case branch to report unsupported socket family
    arguments.
    (checknargs): Throw an error for too many arguments.
    (Pbind, Pconnect, Psendto): Use it.
    Revert to reporting invalid socket address if sockaddr_from_lua
    fails without diagnosing a type error.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- specs/spec_helper.lua
+++ specs/spec_helper.lua
@@ -12,6 +12,63 @@ package.cpath = std.package.normalize ("ext/curses/.libs/?.so",
 posix = require "posix"
 
 
+-- Error message specifications use this to shorten argument lists.
+local function bind (f, fix)
+  return function (...)
+	   local arg = {}
+	   for i, v in ipairs (fix) do
+	     arg[i] = v
+	   end
+	   local i = 1
+	   for _, v in pairs {...} do
+	     while arg[i] ~= nil do i = i + 1 end
+	     arg[i] = v
+	   end
+	   return f (unpack (arg))
+	 end
+end
+
+
+--- Return a formatted bad argument string.
+-- @tparam table M module table
+-- @string fname base-name of the erroring function
+-- @int i argument number
+-- @string want expected argument type
+-- @string[opt] field name of field being type checked
+-- @string[opt="no value"] got actual argument type
+-- @usage
+--   expect (f ()).to_error (badarg (fname, mname, 1, "function"))
+local function badarg (mname, fname, i, want, field, got)
+  if got == nil then field, got = nil, field end  -- field is optional
+  if want == nil then i, want = i - 1, i end      -- numbers only for narg error
+
+  local fqfname = (mname .. "." .. fname):gsub ("^%.", "")
+
+  if got == nil and type (want) == "number" then
+    local s = "bad argument #%d to '%s' (no more than %d arguments expected, got %d)"
+    return s:format (i + 1, fqfname, i, want)
+  elseif field ~= nil then
+    local s = "bad argument #%d to '%s' (%s expected for field '%s', got %s)"
+    return s:format (i, fqfname, want, field, got or "no value")
+  end
+  return string.format ("bad argument #%d to '%s' (%s expected, got %s)",
+                        i, fqfname, want, got or "no value")
+end
+
+
+--- Initialise custom function and argument error handlers.
+-- @tparam table M module table
+-- @string fname function name to bind
+-- @treturn string *fname*
+-- @treturn function badarg with *M* and *fname* prebound
+-- @treturn function toomanyarg with *M* and *fname* prebound
+-- @usage
+--   f, badarg, toomanyarg = init (M, "posix", "bind")
+function init (M, mname, fname)
+  return M[fname], bind (badarg, {mname, fname})
+end
+
+
 -- Use a consistent template for all temporary files.
 TMPDIR = posix.getenv ("TMPDIR") or "/tmp"
 template = TMPDIR .. "/luaposix-test-XXXXXX"
