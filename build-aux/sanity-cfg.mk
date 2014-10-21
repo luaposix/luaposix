b1103c209e8f8b2c0a280413ade558a90ad31a9e
Gary V. Vaughan <gary@gnu.org>
2014-10-21 19:42:27 +0100

    maint: disable prohibit_stat_st_blocks sanity check.
    * build-aux/sanity-cfg.mk (local-checks-to-skip): Add
    sc_prohibit_stat_st_blocks, because we need to use st_blocks
    here until licensing allows using the gnulib/module/stat-size.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- build-aux/sanity-cfg.mk
+++ build-aux/sanity-cfg.mk
@@ -3,4 +3,7 @@ exclude_file_name_regexp--sc_require_config_h_first = ^ext/include/strlcpy.c|ext
 exclude_file_name_regexp--sc_error_message_uppercase = ^examples/socket.lua$$
 exclude_file_name_regexp--sc_file_system = ^ext/posix/stdio.c$$
 
+## Our license precludes linking with gnulib/lib/stat-size.h :(
+local-checks-to-skip = sc_prohibit_stat_st_blocks
+
 EXTRA_DIST += build-aux/sanity-cfg.mk
