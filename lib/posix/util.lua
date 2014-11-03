c41308d1bb6a97c433db2f00dad14b5e3c871e95
Gary V. Vaughan <gary@gnu.org>
2014-11-03 20:36:07 +0000

    posix: move openpty utility into posix and add argchecks.
    * lib/posix/util.lua (openpty): Move from here...
    * lib/posix.lua (openpty): ...to here.  Add argcheck wrapper.
    * lib/posix/util.lua: Reexport openpty for backwards compatibility.
    * specs/posix_spec.yaml (openpty): Add specs for argchecks.
    * build-aux/config.ld.in (file): Remove lib/posix/util.lua.
    * NEWS: Update.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/posix/util.lua
+++ lib/posix/util.lua
@@ -1,44 +1,10 @@
 --[[
  POSIX library for Lua 5.1/5.2.
- (c) Gary V. Vaughan <gary@vaughan.pe>, 2013-2014
- (c) Reuben Thomas <rrt@sc3d.org> 2010-2013
- (c) Natanael Copa <natanael.copa@gmail.com> 2008-2010
+ (c) Gary V. Vaughan <gary@vaughan.pe>, 2014
 ]]
-------
--- @module posix.util
 
-local M = {}
+-- Backwards compatibility alias.
 
-
---- Open a pseudo-terminal.
--- Based on the glibc function of the same name.
--- FIXME: add support for term and win arguments.
--- @return master file descriptor, or nil on error
--- @return slave file descriptor, or message on error
--- @return file name, or nothing on error
-function M.openpty (term, win)
-  local master, reason = posix.openpt (bit.bor (posix.O_RDWR, posix.O_NOCTTY))
-  if master then
-    local ok
-    ok, reason = posix.grantpt (master)
-    if ok then
-      ok, reason = posix.unlockpt (master)
-      if ok then
-        local slave_name
-        slave_name, reason = posix.ptsname (master)
-        if slave_name then
-          local slave
-          slave, reason = posix.open (slave_name, bit.bor (posix.O_RDWR, posix.O_NOCTTY))
-          if slave then
-            return master, slave, slave_name
-          end
-        end
-      end
-    end
-    posix.close (master)
-  end
-  return nil, reason
-end
-
-
-return M
+return {
+  openpty = require "posix".openpty,
+}
