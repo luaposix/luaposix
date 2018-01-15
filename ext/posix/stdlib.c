fd3af648a8ce5bc6d0647e3bba7304228b8c7584
Gary V. Vaughan <gary@gnu.org>
2018-01-14 16:07:39 -0800

    maint: update copyright statements.
    * LICENSE, build-aux/luke, ext/include/_helpers.c,
    ext/posix/ctype.c, ext/posix/dirent.c, ext/posix/errno.c,
    ext/posix/fcntl.c, ext/posix/fnmatch.c, ext/posix/glob.c,
    ext/posix/grp.c, ext/posix/libgen.c, ext/posix/poll.c,
    ext/posix/posix.c, ext/posix/pwd.c, ext/posix/sched.c,
    ext/posix/signal.c, ext/posix/stdio.c, ext/posix/stdlib.c,
    ext/posix/sys/msg.c, ext/posix/sys/resource.c,
    ext/posix/sys/socket.c, ext/posix/sys/stat.c,
    ext/posix/sys/statvfs.c, ext/posix/sys/time.c,
    ext/posix/sys/times.c, ext/posix/sys/utsname.c,
    ext/posix/sys/wait.c, ext/posix/syslog.c, ext/posix/termio.c,
    ext/posix/time.c, ext/posix/unistd.c, ext/posix/utime.c,
    lib/posix/_argcheck.lua, lib/posix/compat.lua,
    lib/posix/deprecated.lua, lib/posix/init.lua, lib/posix/util.lua:
    Add 2018 to copyright years.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- ext/posix/stdlib.c
+++ ext/posix/stdlib.c
@@ -1,6 +1,6 @@
 /*
  * POSIX library for Lua 5.1, 5.2 & 5.3.
- * Copyright (C) 2013-2017 Gary V. Vaughan
+ * Copyright (C) 2013-2018 Gary V. Vaughan
  * Copyright (C) 2010-2013 Reuben Thomas <rrt@sc3d.org>
  * Copyright (C) 2008-2010 Natanael Copa <natanael.copa@gmail.com>
  * Clean up and bug fixes by Leo Razoumov <slonik.az@gmail.com> 2006-10-11
