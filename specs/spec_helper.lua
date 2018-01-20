68e4716d1c032d94d8a5b8203e7e93fdd95ca572
Gary V. Vaughan <gary@gnu.org>
2018-01-20 11:02:48 -0800

    specs: check for global table symbol leaks.
    * specs/spec_helper.lua (show_apis): New helper function for
    comparing environment table contents.
    * specs/lposix_spec.yaml, specs/posix_compat_spec.yaml,
    specs/posix_ctype_spec.yaml, specs/posix_deprecated_spec.yaml,
    specs/posix_dirent_spec.yaml, specs/posix_fcntl_spec.yaml,
    specs/posix_fnmatch_spec.yaml, specs/posix_glob_spec.yaml,
    specs/posix_grp_spec.yaml, specs/posix_pwd_spec.yaml,
    specs/posix_signal_spec.yaml, specs/posix_spec.yaml,
    specs/posix_stdio_spec.yaml, specs/posix_stdlib_spec.yaml,
    specs/posix_sys_msg_spec.yaml, specs/posix_sys_resource_spec.yaml,
    specs/posix_sys_socket_spec.yaml, specs/posix_sys_stat_spec.yaml,
    specs/posix_sys_statvfs_spec.yaml, specs/posix_sys_time_spec.yaml,
    specs/posix_sys_times_spec.yaml, specs/posix_sys_utsname_spec.yaml,
    specs/posix_syslog_spec.yaml, specs/posix_time_spec.yaml,
    specs/posix_unistd_spec.yaml, specs/posix_version_spec.yaml: Use it
    to ensure no symbols leak into the global table.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- specs/spec_helper.lua
+++ specs/spec_helper.lua
@@ -134,3 +134,120 @@ end
 pack = table.pack or function(...)
   return {n=select("#", ...), ...}
 end
+
+
+local function tabulate_output(code)
+  local proc = luaproc(code)
+  if proc.status ~= 0 then
+    return error (proc.errout)
+  end
+  local r = {}
+  proc.output:gsub('(%S*)[%s]*', function (x)
+    if x ~= '' then
+      r[x] = true
+    end
+  end)
+  return r
+end
+
+
+--- Show changes to tables wrought by a require statement.
+-- There are a few modes to this function, controlled by what named
+-- arguments are given.  Lists new keys in T1 after 'require 'import'`:
+--
+--     show_apis {added_to=T1, by=import}
+--
+-- List keys returned from `require 'import'`, which have the same
+-- value in T1:
+--
+--     show_apis {from=T1, used_by=import}
+--
+-- List keys from `require 'import'`, which are also in T1 but with
+-- a different value:
+--
+--     show_apis {from=T1, enhanced_by=import}
+--
+-- List keys from T2, which are also in T1 but with a different value:
+--
+--     show_apis {from=T1, enhanced_in=T2}
+--
+-- @tparam tabble argt one of the combinations above
+-- @treturn table a list of keys according to criteria above
+function show_apis(argt)
+  local added_to, from, not_in, enhanced_in, enhanced_after, by =
+    argt.added_to, argt.from, argt.not_in, argt.enhanced_in,
+    argt.enhanced_after, argt.by
+
+  if added_to and by then
+    return tabulate_output([[
+      local before, after = {}, {}
+      for k in pairs(]] .. added_to .. [[) do
+        before[k] = true
+      end
+
+      local M = require ']] .. by .. [['
+      for k in pairs(]] .. added_to .. [[) do
+        after[k] = true
+      end
+
+      for k in pairs (after) do
+        if not before[k] then
+          print(k)
+        end
+      end
+    ]])
+
+  elseif from and not_in then
+    return tabulate_output([[
+      local _ENV = require 'std.normalize' {
+        from = ']] .. from .. [[',
+        M = require ']] .. not_in .. [[',
+      }
+
+      for k in pairs(M) do
+        -- M[1] is typically the module namespace name, don't match
+        -- that!
+        if k ~= 1 and from[k] ~= M[k] then
+          print (k)
+        end
+      end
+    ]])
+
+  elseif from and enhanced_in then
+    return tabulate_output([[
+      local _ENV = require 'std.normalize' {
+        from = ']] .. from .. [[',
+        M = require ']] .. enhanced_in .. [[',
+      }
+
+      for k, v in pairs (M) do
+        if from[k] ~= M[k] and from[k] ~= nil then
+          print (k)
+        end
+      end
+    ]])
+
+  elseif from and enhanced_after then
+    return tabulate_output([[
+      local _ENV = require 'std.normalize' {
+        from = ']] .. from .. [[',
+      }
+      local before , after = {}, {}
+      for k, v in pairs (from) do
+        before[k] = v
+      end
+      ]] .. enhanced_after .. [[
+      for k, v in pairs(from) do
+        after[k] = v
+      end
+
+      for k, v in pairs (before) do
+        if after[k] ~= nil and after[k] ~= v then
+          print (k)
+        end
+      end
+    ]])
+  end
+
+  assert (false, 'missing argument to show_apis')
+end
